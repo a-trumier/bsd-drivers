321f3d459cea2eb169b917e5a815e932d882a0cd
Alex Trumier <adtrumier@protonmail.com>
2025-12-05 19:34:07 -0600

    Added zu

--- czero/alexzero.c
+++ czero/alexzero.c
@@ -91,7 +91,7 @@ alexzero_read(struct cdev *dev __unused, struct uio *uio, int ioflag __unused)
 	amt = MIN(uio->uio_resid, 1);
     buf = malloc((amt) * sizeof(char), M_BUF, M_WAITOK | M_ZERO);
     memset(buf, 0, amt);
-    uprintf("Buffer size is %d. Moving\n", amt);
+    uprintf("Buffer size is %zu. Moving\n", amt);
     if ((error = uiomove(buf, amt, uio)) != 0)
         uprintf("uiomove failed!\n");
     uprintf("Got past move. Freeing and moving on\n");
